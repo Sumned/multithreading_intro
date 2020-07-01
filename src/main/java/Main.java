3b1968b7ad8aa11811f9f2f5baf38ea6d159593b
Sumned <vladar91@gmail.com>
2020-07-01 17:41:14 +0300

    empty line removed, initialization replaced to main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,10 @@
 public class Main {
     private static final int START = 0;
     private static final int LIMIT = 100;
-    private static RunImpl run;
-    private static ThreadExt ext;
 
     public static void main(String[] args) {
-        run = new RunImpl(START, LIMIT);
-        ext = new ThreadExt(START, LIMIT);
+        RunImpl run = new RunImpl(START, LIMIT);
+        ThreadExt ext = new ThreadExt(START, LIMIT);
         Thread thread = new Thread(run);
         thread.start();
         ext.start();
