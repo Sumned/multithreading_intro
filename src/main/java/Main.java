bcef724248c61da66a131769938029dbf2c27dcb
Sumned <vladar91@gmail.com>
2020-07-01 17:06:33 +0300

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
 public class Main {
-    private static RunImpl run = new RunImpl();
-    private static ThreadExt ext = new ThreadExt();
+    private static final int START = 0;
+    private static final int LIMIT = 100;
+    private static RunImpl run;
+    private static ThreadExt ext;
 
     public static void main(String[] args) {
+        run = new RunImpl(START, LIMIT);
+        ext = new ThreadExt(START, LIMIT);
         Thread thread = new Thread(run);
         thread.start();
         ext.start();
--- src/main/java/ThreadExt.java
+++ src/main/java/ThreadExt.java
@@ -1,10 +1,20 @@
+import lombok.Data;
 import lombok.extern.log4j.Log4j2;
 
+@Data
 @Log4j2
 public class ThreadExt extends Thread {
+    private int start;
+    private int limit;
+
+    public ThreadExt(int start, int limit) {
+        this.start = start;
+        this.limit = limit;
+    }
+
     @Override
     public void run() {
-        for (int i = 0; i < 100; i++) {
+        for (int i = start; i < limit; i++) {
             log.info("Thread-1 value = " + i);
         }
     }
